8a36b2bb6078e36804a244c1c9e7352be809af53
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-21 07:11:04 +0100

    backend code commit by ENGR. IPAYE

--- app/crud.py
+++ app/crud.py
@@ -9,3 +9,20 @@ def get_devices(db: Session):
 
 def get_device(db: Session, device_id: int):
     return db.query(models.Device).filter(models.Device.id == device_id).first()
+
+
+def toggle_device(db: Session, device_id: int):
+    device = get_device(db, device_id)
+    if device:
+        device.state = not device.state
+        db.commit()
+        db.refresh(device)
+    return device
+
+
+def create_device(db: Session, device: schemas.DeviceCreate):
+    db_device = models.Device(name=device.name, type=device.type)
+    db.add(db_device)
+    db.commit()
+    db.refresh(db_device)
+    return db_device
