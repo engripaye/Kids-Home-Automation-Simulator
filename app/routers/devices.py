75e9f0dbf1f6960642c3a20566227d1e092064d2
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-11-23 06:12:14 +0100

    backend code commit by ENGR. IPAYE

---
+++ app/routers/devices.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app import crud, schemas, database
+
+router = APIRouter(prefix="/devices")
+
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/")
+def read_devices(db: Session = Depends(get_db())):
+    return crud.get_devices(db)
+
+
+@router.post("/")
+def add_device(device: schemas.DeviceCreate, db: Session = Depends(get_db())):
+    return crud.create_device(db, device)
+
+
+@router.post("/{device_id}/toggle")
+def toggle_device(device_id: int, db: Session = Depends(get_db())):
+    return crud.toggle_device(db, device_id)
